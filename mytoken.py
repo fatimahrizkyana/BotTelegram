6c21cefc44f54dadd8c050812242785f29f071d0
fatimahrizkyana <63333007+fatimahrizkyana@users.noreply.github.com>
2020-06-13 22:31:38 +0700

    Add files via upload

---
+++ mytoken.py
@@ -0,0 +1,3 @@
+TOKEN= "1296230809:AAEiD03auK5T_wYyjtu2RJpFvxj4P-HD23I"
+SAPA="Halo Selamat Datang"+" \n << Saya bot bernama Sissy yang akan memberi informasi berupa data siswa >>"\
+     +"\n << Untuk mengetahui informasi silahkan ketik /datasiswa >>"+"\n"
